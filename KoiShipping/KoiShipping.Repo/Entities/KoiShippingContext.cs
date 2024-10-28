f6ec7faa8ebea5140b20dffe13ab6fa7f66fbb6e
Tadnuat <datdptse172916@fpt.edu.vn>
2024-10-28 12:06:00 +0700

    update config database

--- KoiShipping/KoiShipping.Repo/Entities/KoiShippingContext.cs
+++ KoiShipping/KoiShipping.Repo/Entities/KoiShippingContext.cs
@@ -34,7 +34,7 @@ public partial class KoiShippingContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=MSI\\SQLEXPRESS;Database=KoiShipping;User Id=sa;Password=12345;TrustServerCertificate=true");
+        => optionsBuilder.UseSqlServer("Data Source=SQL9001.site4now.net;Initial Catalog=db_aaeae4_sa;User Id=db_aaeae4_sa_admin;Password=ilikes1234");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
